08139d4aeda12670b91f20338787566792657f6c
DrSmooker <dsmooker@gmail.com>
2022-08-02 15:49:06 +0300

    All actions work with DB.

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,31 +1,39 @@
 const DB = require('../models/dbServer')
+const fs = require('fs');
 
 
 
 // @desc    Register a user
 // @route   POST /api/register
 async function register(res,user,pass) {
-    try{
+    fs.readFile('./frontend/register.html', function (err,html){
+        if(err){
+            throw err
+        }
         DB.sendRegistrationDb(user,pass)
-
-        res.writeHead(200, { 'Content-Type': 'application/json' })
-        res.end(JSON.stringify({ message: 'Account created!' }))
-    }catch(error){
-        console.log(error)
-    }
-    
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.write(html)
+        res.end()
+   
+    })
 }
 
 // @desc    Authenticate a user
 // @route   POST /api/login
 async function login(res,user,pass) {
-    try{
+    fs.readFile('./frontend/login.html', function (err,html){
+        if(err){
+            throw err
+        }
         DB.sendLoginDb(user,pass)
-        res.writeHead(200, { 'Content-Type': 'application/json' })
-        res.end(JSON.stringify({ message: 'Logged in successfuly!' }))
-    }catch(error){
-        console.log(error)
-    }
+        res.writeHead(200, { 'Content-Type': 'text/html','Set-Cookie':`${user}`+'='+makeCookie()+'; expires='+new Date(new Date().getTime()+86409000).toUTCString() })
+        res.write(html)
+        res.end()
+        
+    })
+    
+        
+    
     
 }
 
@@ -44,9 +52,9 @@ async function reminders(res) {
 
 // @desc    Create a new reminder
 // @route   POST /api/reminders
-async function reminders(res) {
+async function createReminder(res,title,description) {
     try{
-        DB.sendRemindersDb()
+        DB.sendRemindersDb(title,description)
         res.writeHead(200, { 'Content-Type': 'application/json' })
         res.end(JSON.stringify({ message: 'Logged in successfuly!' }))
     }catch(error){
@@ -57,11 +65,11 @@ async function reminders(res) {
 
 // @desc    Update a reminder
 // @route   POST /api/reminders/:id
-async function reminders(res,id) {
+async function updateReminder(res,id) {
     try{
-        DB.sendRemindersDb(id)
+        DB.updateRemindersDb(id,title,description)
         res.writeHead(200, { 'Content-Type': 'application/json' })
-        res.end(JSON.stringify({ message: 'Logged in successfuly!' }))
+        res.end(JSON.stringify({ message: 'Updated successfuly!' }))
     }catch(error){
         console.log(error)
     }
@@ -70,20 +78,34 @@ async function reminders(res,id) {
 
 // @desc    Delete a reminder
 // @route   DELETE /api/reminders/:id
-async function delReminders(res,id) {
+async function deleteReminder(res,id) {
     try{
-        DB.sendRemindersDb(user,pass)
+        DB.deleteRemindersDb(id)
         res.writeHead(200, { 'Content-Type': 'application/json' })
-        res.end(JSON.stringify({ message: 'Logged in successfuly!' }))
+        res.end(JSON.stringify({ message: 'Deleted successfuly!' }))
     }catch(error){
         console.log(error)
     }
     
 }
 
+function makeCookie() {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = 25;
+    for ( var i = 0; i < 25; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
+
 module.exports = {
     register,
     login,
     reminders,
-    delReminders
+    createReminder,
+    updateReminder,
+    deleteReminder
+    
 }
